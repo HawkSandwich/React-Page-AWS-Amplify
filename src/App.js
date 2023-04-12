875532d8bb9287dfa23264f8b937f59162344570
unknown <hawksandwichgames@gmail.com>
2023-04-11 21:34:42 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to TeeTotalTab.com</h1>
+      <h3>No content has been developed yet, but the website is running!</h3>
+    </div>
+  );
+}
+
+export default App;
